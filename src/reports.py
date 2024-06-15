631618065bcf31d41925919d364c5b9062b9f920
DiannaaaM <murlaevadiana@gmail.com>
2024-06-15 12:22:46 +0300

    1

--- src/reports.py
+++ src/reports.py
@@ -1,13 +1,19 @@
 from src.utils import reader_operations
 import pandas as pd
+from typing import Optional
 
+def wastes_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None):
+    """
 
-def wastes_by_category(transactions: pd.DataFrame, category: str):
-    total = 0.0
-    for transaction in transactions:
-        if transaction["Категория"] == category:
-            total += transaction["Сумма операции"] * -1
+    """
+    if date is None:
+        date = pd.to_datetime("today")
+    else:
+        date = pd.to_datetime(date)
+    transactions = transactions.query("Дата операции >= @date - pd.DateOffset(months=3)")
+    total = -transactions[transactions["Категория"] == category]["Сумма_операции"].sum()
     return round(total, 1)
 
 
 print(wastes_by_category(reader_operations, "Супермаркеты"))
+print(wastes_by_category(reader_operations, "Супермаркеты", "2021-01-01"))
